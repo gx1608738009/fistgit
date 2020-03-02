90ee09d71ce23a0f8f0f3d4ddd75baef1f249c65
Thinkpad <Thinkpad@DESKTOP-SSSI71V>
2020-03-02 16:13:39 +0800

    第一次提交

---
+++ src/dao/impl/UserDaoImpl.java
@@ -0,0 +1,25 @@
+package dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.UserDao;
+/**
+ * 实现dao中方法的一个操作类
+ */
+import entity.User;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void save(User user) {
+		// 创建一个集合来保存用户信息
+        List<User> list=new ArrayList<User>();
+        list.add(user);
+        for(User user2:list) {
+        	System.out.println(user2.getName()+","
+        			+user2.getAge());
+        }
+	}
+
+}
